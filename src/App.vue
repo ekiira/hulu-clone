9e19243ee985383195feed7537a6fce7e634429c
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2022-02-10 01:50:10 +0100

    remove default components

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="wrapper">
+      <img alt="Vue logo" class="logo" src="./assets/hulu-logo.svg" />
+    </div>
   </main>
 </template>
 
 <style>
-@import './assets/base.css';
+@import "./assets/base.css";
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: auto;
 }
 
 a,
@@ -50,32 +37,10 @@ a,
   }
 }
 
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
+@media (max-width: 1023px) {
   .logo {
-    margin: 0 2rem 0 0;
+    width: 300px;
+    height: 300px;
   }
 }
 </style>
