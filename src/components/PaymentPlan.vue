810710c27753e76d8bba8b13b8111cc24a5b4f18
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2022-05-06 23:41:03 +0100

    adjust payment toggle size

--- src/components/PaymentPlan.vue
+++ src/components/PaymentPlan.vue
@@ -64,13 +64,13 @@ const showAddons = ref(false);
                 <Switch
                   v-model="enabledSave"
                   :class="enabledSave ? 'bg-green-1' : 'bg-gray-9'"
-                  class="relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                  class="relative inline-flex h-[32px] w-[68px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
                 >
                   <span class="sr-only">Use setting</span>
                   <span
                     aria-hidden="true"
                     :class="enabledSave ? 'translate-x-9' : 'translate-x-0'"
-                    class="pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+                    class="pointer-events-none inline-block h-[28px] w-[28px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
                   />
                 </Switch>
               </div>
